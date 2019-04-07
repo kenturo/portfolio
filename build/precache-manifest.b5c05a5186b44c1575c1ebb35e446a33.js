8ae7979bdb2ed34bd1cfffd8197194472b1be067
kiet.dang <kiet.dang@paradiseio.com>
2019-04-07 16:29:52 +0700

    git commit build

---
+++ build/precache-manifest.b5c05a5186b44c1575c1ebb35e446a33.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d2884dceb9e051272aca8de70e8c21a",
+    "url": "/static/media/company.0d2884dc.jpg"
+  },
+  {
+    "revision": "fe67e6f082c90f9fa29b7a6909c35675",
+    "url": "/static/media/coding-min.fe67e6f0.jpg"
+  },
+  {
+    "revision": "7b2f7fd11aa124749853d2789604b7b5",
+    "url": "/static/media/bgContact.7b2f7fd1.jpg"
+  },
+  {
+    "revision": "4ff0c61bfc017dc9106d26464f8b02b4",
+    "url": "/static/media/bg-game.4ff0c61b.gif"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "889cc99e2d0b506895f5",
+    "url": "/static/js/main.25e3d75b.chunk.js"
+  },
+  {
+    "revision": "19498516a5f66012e66b",
+    "url": "/static/js/2.7e204fc5.chunk.js"
+  },
+  {
+    "revision": "889cc99e2d0b506895f5",
+    "url": "/static/css/main.607a8ee1.chunk.css"
+  },
+  {
+    "revision": "19498516a5f66012e66b",
+    "url": "/static/css/2.e1f64428.chunk.css"
+  },
+  {
+    "revision": "08c23a17e5ec28ce56b4a67fe3f92fdc",
+    "url": "/index.html"
+  }
+];
